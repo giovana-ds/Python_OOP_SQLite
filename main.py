f1c78915d807f70a8b7895f5714eded77782c848
Giovana da Silveira <102772845+giovana-ds@users.noreply.github.com>
2022-11-23 23:56:56 +0000

    exemplo simples de orientação a objetos

---
+++ main.py
@@ -0,0 +1,9 @@
+# importar Pessoa.py para diretório model
+from model.Pessoa import Pessoa
+
+# exemplo de uso
+giovana = Pessoa(1, "Giovana Silveira")
+print(giovana)
+
+# mostrar só o nome
+print(giovana.nome)
